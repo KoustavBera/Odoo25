4ad21d7adb34aeb601699c90fd5c830dff9f9ef7
KoustavBera <thekoustavofficial@gmail.com>
2025-07-12 14:51:54 +0530

    frontend 18%

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import StackItHomepage from "./StackItHomepage.jsx";
 import AuthPages from "./pages/AuthPages.jsx";
 import Navbar from "../components/Navbar.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+
 function App() {
   return (
     <>
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<StackItHomepage />} />
         <Route path="/auth" element={<AuthPages />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   );
